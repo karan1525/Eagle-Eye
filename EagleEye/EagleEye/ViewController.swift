0393b1afab1f9e8b21180d35ae4802da92cf5fa8
karan1525 <karan1525@yahoo.co.in>
2017-11-18 12:16:36 -0800

    Added point based system

--- EagleEye/EagleEye/ViewController.swift
+++ EagleEye/EagleEye/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     
     var currentValue: Int = 50
     var targetValue: Int = 50
+    var score: Int = 0
+    var round: Int = 0
+
     @IBOutlet weak var slider: UISlider! //always form a weak connection
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,15 +38,37 @@ class ViewController: UIViewController {
         currentValue = 50
         targetValue = 1 + Int(arc4random_uniform(100))
         slider.value = Float(currentValue)
+        round += 1
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     
     @IBAction func showAction() {
-        let message: String = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
-        let alert = UIAlertController(title: "Hi there!",
+        let difference = abs(targetValue - currentValue)
+        var points: Int = 100 - difference
+        
+        
+        let title: String
+        if difference == 0 {
+            title = "You got eagle eyes!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it"
+            points += 50
+        } else if difference < 10 {
+            title = "Pretty good"
+        } else {
+            title = "Not so good"
+        }
+        
+        score += points
+        
+        let message: String = "You scored \(points) points"
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         
