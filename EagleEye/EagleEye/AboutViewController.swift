14a1e8254b4f94051c423a8eed82695608e675e7
karan1525 <karan1525@yahoo.co.in>
2017-11-18 12:37:35 -0800

    Added about page viewController

---
+++ EagleEye/EagleEye/AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  EagleEye
+//
+//  Created by Karan Bhargava on 11/18/17.
+//  Copyright © 2017 Karan Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
