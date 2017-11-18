9396a91e8ce38ed69a7ceb18f7fa0ba8dcbe546a
karan1525 <karan1525@yahoo.co.in>
2017-11-18 11:43:51 -0800

    New Round Start code added

--- EagleEye/EagleEye/ViewController.swift
+++ EagleEye/EagleEye/ViewController.swift
@@ -11,20 +11,30 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 50
+    var targetValue: Int = 50
     @IBOutlet weak var slider: UISlider! //always form a weak connection
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
+    func startNewRound() {
+        currentValue = 50
+        targetValue = 1 + Int(arc4random_uniform(100))
+        slider.value = Float(currentValue)
+    }
+    
     @IBAction func showAction() {
-        let message: String = "The Value of the slider is: \(currentValue)"
+        let message: String = "The Value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hi there!",
                                       message: message,
                                       preferredStyle: .alert)
@@ -33,8 +43,9 @@ class ViewController: UIViewController {
                                    style: .default,
                                    handler: nil)
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
